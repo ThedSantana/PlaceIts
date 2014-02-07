45933040ce7a86870e359c97eaed8d6a67c48ce6
Andrew Du <aadu@ucsd.edu>
2014-02-07 11:34:12 -0800

    Version 1.1
    Added daychooser implementation

--- src/com/cs110/team10/placeits/TestActivity.java
+++ src/com/cs110/team10/placeits/TestActivity.java
@@ -1,26 +1,27 @@
 package com.cs110.team10.placeits;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.GoogleMap.CancelableCallback;
 import com.google.android.gms.maps.GoogleMap.OnMapClickListener;
-import com.google.android.gms.maps.GoogleMap.OnMapLongClickListener;
 import com.google.android.gms.maps.GoogleMap.OnMarkerClickListener;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.model.LatLng;
@@ -28,9 +29,14 @@ import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class TestActivity extends Activity implements OnMapClickListener, OnMarkerClickListener, CancelableCallback{
-	 private GoogleMap googleMap;
-	 private HashMap<Marker, String> markerList;
+	 private static GoogleMap googleMap;
+	 private Map<Marker, String> markerList;
 	 private static boolean addMarker = false;
+	 private Marker added;                   // Temp Marker used for onActivityResult()
+	 private LatLng tempPos;				// Temp position used for onActivityResult()
+	 private String tempValue;              // Temp string used for onActvityResult()
+
+		
 	 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -96,31 +102,51 @@ public class TestActivity extends Activity implements OnMapClickListener, OnMark
 		
 
 		final LatLng pos = position;
+		tempPos = pos;
 		AlertDialog.Builder alert = new AlertDialog.Builder(this);
 		alert.setTitle("New Marker");
 		alert.setMessage("Please enter a Marker Title:");
 		// Set an EditText view to get user input
 		final EditText input = new EditText(this);
 		alert.setView(input);
+		
 		alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
 			public void onClick(DialogInterface dialog, int whichButton) {
-				String value = input.getText().toString();
-				Toast.makeText(TestActivity.this, "Tag added!",
-						Toast.LENGTH_SHORT).show();
-				Marker added = googleMap.addMarker(new MarkerOptions().position(pos)
-						.title(value));
-				markerList.put(added, value);
-
-			}
+				// Do nothing, this just overrides the code so that
+				// when OK is clicked with an EMPTY text field, the dialog doesn't
+				// automatically close.
+				
+				}
 		});
+		
 		alert.setNegativeButton("Cancel",
 				new DialogInterface.OnClickListener() {
 					public void onClick(DialogInterface dialog, int whichButton) {
-						Toast.makeText(TestActivity.this, "Nothing added!",
-								Toast.LENGTH_SHORT).show();
+						Toast.makeText(TestActivity.this, "Nothing added!", Toast.LENGTH_SHORT).show();
 					}
-				});
-		alert.show();
+		});
+		
+		final AlertDialog alertDialog = alert.create();
+		alertDialog.show();
+		alertDialog.getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener(new OnClickListener() {
+			
+
+			@Override
+			public void onClick(View v) {
+				String value = input.getText().toString();
+				tempValue = value;
+				// Checks if the users inputs an empty text field when adding a note
+				if(value.isEmpty()){
+					Toast.makeText(TestActivity.this, "Please add some text", Toast.LENGTH_SHORT).show();
+				}else{
+					Intent intent = new Intent(TestActivity.this, DayChooser.class);
+					startActivityForResult(intent, 1);
+					
+					alertDialog.dismiss();
+				}
+			}
+		});
+		
 	}
 	
 	@Override
@@ -130,19 +156,21 @@ public class TestActivity extends Activity implements OnMapClickListener, OnMark
 		AlertDialog.Builder alert = new AlertDialog.Builder(this);
 		alert.setMessage(markerList.get(marker));
 		// Set an EditText view to get user input
-		alert.setPositiveButton("Remove Marker", new DialogInterface.OnClickListener() {
+		alert.setPositiveButton("Remove Note", new DialogInterface.OnClickListener() {
 			public void onClick(DialogInterface dialog, int whichButton) {
 				marker.remove();
-				Toast.makeText(TestActivity.this, "Marker removed!", Toast.LENGTH_SHORT).show();
+				Toast.makeText(TestActivity.this, "Note removed!", Toast.LENGTH_SHORT).show();
 
 			}
 		});
+		
 		alert.setNegativeButton("Cancel",
 				new DialogInterface.OnClickListener() {
 					public void onClick(DialogInterface dialog, int whichButton) {
-						
+						// Do nothing because the user clicked cancel
 					}
-				});
+		});
+		
 		alert.show();
 		return true;
 	}
@@ -154,12 +182,30 @@ public class TestActivity extends Activity implements OnMapClickListener, OnMark
 		return super.onCreateOptionsMenu(menu);
 	}
 	
-	 @Override
-	    protected void onResume() {
-	        super.onResume();
-	        initializeMap();
-	    }
+	@Override
+	protected void onResume() {
+		super.onResume();
+		initializeMap();
+
+	}
 	
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 1) {
+            if (resultCode == RESULT_OK) {
+        		added = googleMap.addMarker(new MarkerOptions().position(tempPos).title(tempValue));
+				markerList.put(added, tempValue);
+            }
+        }
+    }
+
+	 
+	 private static void addAllMarkers(Map<Marker, String> map) {
+		 for (Entry<Marker, String> entry : map.entrySet()) {
+			    Marker key = entry.getKey();
+			    String value = entry.getValue();
+			    googleMap.addMarker(new MarkerOptions().position(key.getPosition()).title(value));
+			}
+		}
 	 /**
      * function to load map. If map is not created it will create it for you
      * */
@@ -177,6 +223,20 @@ public class TestActivity extends Activity implements OnMapClickListener, OnMark
         }
     } // initializeMap()
 
+    /*
+     * Used to re-add all markers when orientation is changed. 
+     */
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+    	Log.d("TestActivity", "wee woo");
+
+        
+        if(!markerList.isEmpty()){
+        	addAllMarkers(markerList);
+        }
+        
+    }
 
 	@Override
 	public void onCancel() {
